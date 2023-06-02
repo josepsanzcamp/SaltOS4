0a9098721e0c9596fcf3b1cb4b730b57433f23af
Josep Sanz <josep.sanz@saltos.org>
2023-06-02 11:31:08 +0000

    Cambiar como se usa phpcs con los js para que sea mas practico

--- scripts/phpcs_js.php
+++ scripts/phpcs_js.php
@@ -1,13 +1,12 @@
 <?php
 
-if ($argc != 4) {
-    echo "Error en los argumentos, debe ser fichero js, fichero de cache y accion (a/b)\n";
+if ($argc != 3) {
+    echo "Error en los argumentos, debe ser fichero antiguo js y fichero nuevo js\n";
     die();
 }
 
 $file1 = $argv[1];
 $file2 = $argv[2];
-$accion = $argv[3];
 
 $buffer = file_get_contents($file1);
 $buffer = explode("`", $buffer);
@@ -18,26 +17,11 @@ if ($count % 2 == 0) {
     $pos++;
 }
 
-if ($accion == "a") {
-    $cache = array();
-    for ($i = $pos; $i < $count; $i += 2) {
-        $hash = md5($buffer[$i]);
-        $key = "__HASH_${hash}__";
-        $cache[$hash] = $buffer[$i];
-        $buffer[$i] = $key;
-    }
-    file_put_contents($file2, serialize($cache));
-}
-
-if ($accion == "b") {
-    $cache = unserialize(file_get_contents($file2));
-    for ($i = $pos; $i < $count; $i += 2) {
-        $key = $buffer[$i];
-        $hash = str_replace(array("__HASH_","__"), "", $key);
-        $buffer[$i] = $cache[$hash];
-    }
-    unlink($file2);
+for ($i = $pos; $i < $count; $i += 2) {
+    $hash = md5($buffer[$i]);
+    $key = "__HASH_${hash}__";
+    $buffer[$i] = $key;
 }
 
 $buffer = implode("`", $buffer);
-file_put_contents($file1, $buffer);
+file_put_contents($file2, $buffer);
--- scripts/phpcs_js.sh
+++ scripts/phpcs_js.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-php ../scripts/phpcs_js.php $1 $1.cache a
-phpcs $1
-php ../scripts/phpcs_js.php $1 $1.cache b
+php ../scripts/phpcs_js.php $1 $1.phpcs.js
+phpcs $1.phpcs.js
+rm -f $1.phpcs.js
