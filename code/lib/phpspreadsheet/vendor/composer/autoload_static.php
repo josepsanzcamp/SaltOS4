99f19da25babb98c399fcaf3829e39776ca7d86c
Josep Sanz <josep.sanz@saltos.org>
2023-08-01 13:33:57 +0000

    Actualizar class loader del composer

--- code/lib/phpspreadsheet/vendor/composer/autoload_static.php
+++ code/lib/phpspreadsheet/vendor/composer/autoload_static.php
@@ -43,8 +43,8 @@ class ComposerStaticInit285d4d93436e04b34dfccec8b0c18014
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
