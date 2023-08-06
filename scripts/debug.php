ec03f1b9edcae64429942ce40bf0e84a0efc07b4
Josep Sanz <josep.sanz@saltos.org>
2023-08-06 10:59:13 +0000

    Pertenece al commit anterior

--- scripts/debug.php
+++ scripts/debug.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 $buffer = file_get_contents("php://stdin");
 $buffer = explode("\n", $buffer);
 foreach ($buffer as $key => $val) {
-    if (strpos($val, "js/index.min.js") === false) {
+    if (strpos($val, "index.min.js") === false) {
         continue;
     }
     array_shift($argv);
     $buffer[$key] = [];
     while ($temp = array_shift($argv)) {
-        $buffer[$key][] = str_replace("js/index.min.js", $temp, $val);
+        $buffer[$key][] = str_replace("core/js/index.min.js", $temp, $val);
     }
     $buffer[$key] = implode("\n", $buffer[$key]);
 }
