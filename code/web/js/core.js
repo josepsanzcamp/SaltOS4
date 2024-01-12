24cfdc09a5239877ab9f8a00d9bc93b3b1fc4387
Josep Sanz <josep.sanz@saltos.org>
2024-01-12 06:14:05 +0000

    Fix an issue with javascript modules

--- code/web/js/core.js
+++ code/web/js/core.js
@@ -426,7 +426,8 @@ saltos.core.require = file => {
     if (file.substr(-4) == '.mjs' || file.includes('.mjs?')) {
         var script = document.createElement('script');
         script.type = 'module';
-        script.innerHTML = ajax.response;
+        script.src = file;
+        script.async = false;
         document.body.append(script);
     }
     // To prevent duplicates
