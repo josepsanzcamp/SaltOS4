dac319a17bd6567eb92919e348af1f7cf0d46d1d
Josep Sanz <josep.sanz@saltos.org>
2024-08-21 08:40:04 +0000

    Added a helper module to retrieve the browser info

---
+++ code/api/php/lib/browser.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ *  ____        _ _    ___  ____    _  _    ___
+ * / ___|  __ _| | |_ / _ \/ ___|  | || |  / _ \
+ * \___ \ / _` | | __| | | \___ \  | || |_| | | |
+ *  ___) | (_| | | |_| |_| |___) | |__   _| |_| |
+ * |____/ \__,_|_|\__|\___/|____/     |_|(_)___/
+ *
+ * SaltOS: Framework to develop Rich Internet Applications
+ * Copyright (C) 2007-2024 by Josep Sanz Campderrós
+ * More information in https://www.saltos.org or info@saltos.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+/**
+ * Browser helper module
+ *
+ * This file contain useful browser helper functions
+ */
+
+/**
+ * Get Browser Platform Device Type
+ *
+ * This function gets the browser, platform and device_type form the user_agent header
+ */
+function get_browser_platform_device_type($user_agent = null)
+{
+    $default = array_fill_keys([
+        "browser",
+        "platform",
+        "device_type",
+    ], "unknown");
+    $browscap = ini_get("browscap");
+    if (!$browscap) {
+        return $default;
+    }
+    $array = get_browser($user_agent, true);
+    $array = array_intersect_key($array, $default);
+    return $array;
+}
