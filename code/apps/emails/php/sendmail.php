0af4eddb038049842bbb6741ae34d94a20d12aa9
Josep Sanz <josep.sanz@saltos.org>
2024-09-08 04:31:07 +0000

    Add the same filters features to sendmail like getmail

--- code/apps/emails/php/sendmail.php
+++ code/apps/emails/php/sendmail.php
@@ -688,7 +688,9 @@ function sendmail_prepare($action, $email_id)
                         $temp = remove_style_tag($temp);
                         $temp = remove_comment_tag($temp);
                         $temp = remove_meta_tag($temp);
+                        $temp = remove_link_tag($temp);
                         $temp = inline_img_tag($temp);
+                        $temp = inline_img_style($temp);
                     }
                     foreach ($result2 as $index2 => $node2) {
                         $disp2 = $node2['disp'];
