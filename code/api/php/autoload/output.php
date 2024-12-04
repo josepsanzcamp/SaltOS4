746e3ac521b94b5f89d0b5fb4cbbb574755d410e
Josep Sanz <josep.sanz@saltos.org>
2024-12-04 20:09:30 +0000

    Add some parameters to improve the compression speed vs size

--- code/api/php/autoload/output.php
+++ code/api/php/autoload/output.php
@@ -96,13 +96,16 @@ function output_handler($array)
         $encoding = strval(get_server('HTTP_ACCEPT_ENCODING'));
         if (stripos($encoding, 'zstd') !== false && function_exists('zstd_compress')) {
             __output_header('Content-Encoding: zstd');
-            $data = zstd_compress($data);
+            $data = zstd_compress($data, 1);
+        //~ } elseif (stripos($encoding, 'br') !== false && function_exists('brotli_compress')) {
+            //~ __output_header('Content-Encoding: br');
+            //~ $data = brotli_compress($data, 0);
         } elseif (stripos($encoding, 'gzip') !== false && function_exists('gzencode')) {
             __output_header('Content-Encoding: gzip');
-            $data = gzencode($data);
+            $data = gzencode($data, 0);
         } elseif (stripos($encoding, 'deflate') !== false && function_exists('gzdeflate')) {
             __output_header('Content-Encoding: deflate');
-            $data = gzdeflate($data);
+            $data = gzdeflate($data, 0);
         } else {
             __output_header('Content-Encoding: identity');
         }
