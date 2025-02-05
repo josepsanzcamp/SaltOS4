ba8c05761878b849a677c8c93aae1918a00d5fab
Josep Sanz <josep.sanz@saltos.org>
2025-02-05 16:38:52 +0000

    Add the nssdb utest with some pdfs as example of usage

---
+++ utest/test_nssdb.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+ *  ____        _ _    ___  ____    _  _    ___
+ * / ___|  __ _| | |_ / _ \/ ___|  | || |  / _ \
+ * \___ \ / _` | | __| | | \___ \  | || |_| | | |
+ *  ___) | (_| | | |_| |_| |___) | |__   _| |_| |
+ * |____/ \__,_|_|\__|\___/|____/     |_|(_)___/
+ *
+ * SaltOS: Framework to develop Rich Internet Applications
+ * Copyright (C) 2007-2025 by Josep Sanz Campderrós
+ * More information in https://www.saltos.org or info@saltos.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+// phpcs:disable PSR1.Classes.ClassDeclaration
+// phpcs:disable Squiz.Classes.ValidClassName
+// phpcs:disable PSR1.Methods.CamelCapsMethodName
+// phpcs:disable PSR1.Files.SideEffects
+
+/**
+ * Test nssdb
+ *
+ * This test performs some tests to validate the correctness
+ * of the nssdb functions
+ */
+
+/**
+ * Importing namespaces
+ */
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\Attributes\Depends;
+
+/**
+ * Loading helper function
+ *
+ * This file contains the needed function used by the unit tests
+ */
+require_once 'php/lib/nssdb.php';
+
+/**
+ * Main class of this unit test
+ */
+final class test_nssdb extends TestCase
+{
+    #[testdox('nssdb functions')]
+    /**
+     * nssdb test
+     *
+     * This test performs some tests to validate the correctness
+     * of the nssdb functions
+     */
+    public function test_nssdb(): void
+    {
+        $this->assertDirectoryDoesNotExist('data/files/nssdb');
+        $this->assertSame(__nssdb_init(), []);
+        $this->assertDirectoryExists('data/files/nssdb');
+        $this->assertSame(__nssdb_list(), ['There are no certificates available.']);
+
+        $certfile = 'data/files/certificate.p12';
+        $this->assertFileDoesNotExist($certfile);
+        $this->assertMatchesRegularExpression('/^[+.*]+$/', implode('', __nssdb_create($certfile, '1234')));
+        $this->assertFileExists($certfile);
+
+        $this->assertSame(__nssdb_add($certfile, '1234'), ['pk12util: PKCS12 IMPORT SUCCESSFUL']);
+        $info = __nssdb_list();
+        $this->assertIsArray($info);
+        $this->assertCount(2, $info);
+        $this->assertSame($info[0], 'Certificate nicknames available:');
+        $this->assertSame($info[1], 'THE SALTOS PROJECT - 12345678X');
+
+        unlink($certfile);
+        $nick = $info[1];
+
+        $this->assertSame($nick, 'THE SALTOS PROJECT - 12345678X');
+        $info = __nssdb_info($nick);
+        $this->assertIsArray($info);
+        $this->assertCount(6, $info);
+        $this->assertSame($info['countryName'], 'ES');
+        $this->assertSame($info['serialNumber'], 'ABCDE-12345678X');
+        $this->assertSame($info['organizationName'], '12345678X');
+        $this->assertSame($info['commonName'], 'THE SALTOS PROJECT');
+        $this->assertArrayHasKey('sha1', $info);
+        $this->assertArrayHasKey('sha256', $info);
+
+        $input = '../../utest/files/input1.pdf';
+        $middle = get_cache_file($input, '.pdf');
+        $output = 'data/files/output1.pdf';
+
+        $this->assertFileDoesNotExist($middle);
+        $this->assertSame(__nssdb_update($nick, $input), $middle);
+        $this->assertFileExists($middle);
+
+        $this->assertFileDoesNotExist($output);
+        $info = __nssdb_pdfsig($nick, $middle, $output);
+        $this->assertFileExists($output);
+
+        $this->assertIsArray($info);
+        $this->assertSame($info[0], "Digital Signature Info of: $output");
+        $this->assertSame($info[1], 'Signature #1:');
+        $this->assertStringContainsString('ABCDE-12345678X', implode('', $info));
+        $this->assertStringContainsString('12345678X', implode('', $info));
+        $this->assertStringContainsString('THE SALTOS PROJECT', implode('', $info));
+
+        $this->assertGreaterThan(filesize($input), filesize($middle));
+        $this->assertGreaterThan(filesize($middle), filesize($output));
+        unlink($middle);
+        unlink($output);
+
+        $input = '../../utest/files/input2.pdf';
+        $middle = get_cache_file($input, '.pdf');
+        $output = 'data/files/output2.pdf';
+
+        $this->assertFileDoesNotExist($middle);
+        $this->assertSame(__nssdb_update($nick, $input), $middle);
+        $this->assertFileExists($middle);
+
+        $this->assertFileDoesNotExist($output);
+        $info = __nssdb_pdfsig($nick, $middle, $output);
+        $this->assertFileExists($output);
+
+        $this->assertIsArray($info);
+        $this->assertSame($info[0], "Digital Signature Info of: $output");
+        $this->assertSame($info[1], 'Signature #1:');
+        $this->assertStringContainsString('ABCDE-12345678X', implode('', $info));
+        $this->assertStringContainsString('12345678X', implode('', $info));
+        $this->assertStringContainsString('THE SALTOS PROJECT', implode('', $info));
+
+        $this->assertGreaterThan(filesize($input), filesize($middle));
+        $this->assertGreaterThan(filesize($middle), filesize($output));
+        unlink($middle);
+        unlink($output);
+
+        $this->assertSame(__nssdb_remove($nick), []);
+        $this->assertSame(__nssdb_list(), ['There are no certificates available.']);
+
+        $this->assertSame(__nssdb_reset(), []);
+        $this->assertSame(__nssdb_list(), ['There are no certificates available.']);
+    }
+}
