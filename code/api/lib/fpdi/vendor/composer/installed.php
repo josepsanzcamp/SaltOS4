66d7bae79b52c69c8a2171adf3e1d862a886f1a1
Josep Sanz <josep.sanz@saltos.org>
2025-02-05 15:30:51 +0000

    Updated the fpdi library to the latest release

--- code/api/lib/fpdi/vendor/composer/installed.php
+++ code/api/lib/fpdi/vendor/composer/installed.php
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'setasign/fpdi' => array(
-            'pretty_version' => 'v2.6.2',
-            'version' => '2.6.2.0',
-            'reference' => '9e013b376939c0d4029f54150d2a16f3c67a5797',
+            'pretty_version' => 'v2.6.3',
+            'version' => '2.6.3.0',
+            'reference' => '67c31f5e50c93c20579ca9e23035d8c540b51941',
             'type' => 'library',
             'install_path' => __DIR__ . '/../setasign/fpdi',
             'aliases' => array(),
