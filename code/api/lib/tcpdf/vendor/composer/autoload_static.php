2f05e533390d92e924fa7cb10073d685509b8e9e
Josep Sanz <josep.sanz@saltos.org>
2025-03-30 17:53:30 +0000

    Updated the tcpdf and pdfjs libraries to the latest release

--- code/api/lib/tcpdf/vendor/composer/autoload_static.php
+++ code/api/lib/tcpdf/vendor/composer/autoload_static.php
@@ -19,8 +19,6 @@ class ComposerStaticInit1ba944b1b2880695113851e981334169
         'TCPDF_FONTS' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/tcpdf_fonts.php',
         'TCPDF_FONT_DATA' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/tcpdf_font_data.php',
         'TCPDF_IMAGES' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/tcpdf_images.php',
-        'TCPDF_IMPORT' => __DIR__ . '/..' . '/tecnickcom/tcpdf/tcpdf_import.php',
-        'TCPDF_PARSER' => __DIR__ . '/..' . '/tecnickcom/tcpdf/tcpdf_parser.php',
         'TCPDF_STATIC' => __DIR__ . '/..' . '/tecnickcom/tcpdf/include/tcpdf_static.php',
     );
 
--- code/api/lib/tcpdf/vendor/composer/installed.php
+++ code/api/lib/tcpdf/vendor/composer/installed.php
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'tecnickcom/tcpdf' => array(
-            'pretty_version' => '6.8.2',
-            'version' => '6.8.2.0',
-            'reference' => 'f7a781073e1645062f163e058139e2f89355d420',
+            'pretty_version' => '6.9.0',
+            'version' => '6.9.0.0',
+            'reference' => 'f67b761b61f2370a9000d98c3b9111284544f722',
             'type' => 'library',
             'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
             'aliases' => array(),
