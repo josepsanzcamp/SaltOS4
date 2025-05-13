462923d57557e4c30bbb3aebbef9ea2a0ce130b4
Josep Sanz <josep.sanz@saltos.org>
2025-05-13 19:12:48 +0000

    Update the tcpdf library to the latest release

--- code/api/lib/tcpdf/vendor/composer/installed.php
+++ code/api/lib/tcpdf/vendor/composer/installed.php
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'tecnickcom/tcpdf' => array(
-            'pretty_version' => '6.9.3',
-            'version' => '6.9.3.0',
-            'reference' => 'd0e8dd17f8a1df3c97b94b9af8356e88e811ed59',
+            'pretty_version' => '6.9.4',
+            'version' => '6.9.4.0',
+            'reference' => 'c838d7f7babb0d35763acfb9ecf78c3f45966f83',
             'type' => 'library',
             'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
             'aliases' => array(),
