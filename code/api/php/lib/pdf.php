8b7f43087267b9ac9658201ff3c89361d08ebc50
Josep Sanz <josep.sanz@saltos.org>
2025-05-14 20:31:06 +0000

    Improve some things related to the previous commit

--- code/api/php/lib/pdf.php
+++ code/api/php/lib/pdf.php
@@ -38,6 +38,8 @@ declare(strict_types=1);
  * including custom PDF class extensions and various PDF manipulation utilities
  */
 
+define('K_TCPDF_EXTERNAL_CONFIG', true);// to prevent the load of tcpdf_config.php
+define('K_TCPDF_THROW_EXCEPTION_ERROR', true); // to force a throw exception on error
 define('K_PATH_IMAGES', ''); // to prevent open_basedir restriction
 require_once 'lib/tcpdf/vendor/autoload.php';
 
