b16c78c47baf094fb1da84d29f9b917be47224f5
Josep Sanz <josep.sanz@saltos.org>
2025-06-17 04:22:30 +0000

    Move the detect_app_file functionality inside the get_order_fields

--- code/apps/common/php/order.php
+++ code/apps/common/php/order.php
@@ -34,16 +34,17 @@ declare(strict_types=1);
 /**
  * Get Order Fields
  *
- * Get Orderable Fields from XML Definition, to do it, parses a given XML file
- * and extracts the list of field names that are marked as orderable.
+ * Get Orderable Fields from the XML Definition, to do it, parses a given
+ * XML file and extracts the list of field names that are marked as orderable.
  *
- * @xmlfile => Path to the XML file containing the layout definition.
+ * @app => app to search the xml file that contains the layout definition.
  *
  * Returns an array of field names that have the 'order' attribute enabled.
  */
-function get_order_fields($xmlfile)
+function get_order_fields($app)
 {
-    $array = xmlfile2array($xmlfile);
+    $file = detect_app_file($app);
+    $array = xmlfile2array($file);
     $array = xpath_search_first_value('list[id=cache]/layout/row/table/header', $array);
     $fields = [];
     foreach ($array as $key => $val) {
