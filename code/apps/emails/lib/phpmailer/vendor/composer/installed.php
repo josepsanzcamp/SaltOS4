38351d0d10464c1e4518d9c94fdde5960965e394
Josep Sanz <josep.sanz@saltos.org>
2025-10-15 20:02:20 +0000

    Update phpmailer to the latest release

--- code/apps/emails/lib/phpmailer/vendor/composer/installed.php
+++ code/apps/emails/lib/phpmailer/vendor/composer/installed.php
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'phpmailer/phpmailer' => array(
-            'pretty_version' => 'v6.11.1',
-            'version' => '6.11.1.0',
-            'reference' => 'd9e3b36b47f04b497a0164c5a20f92acb4593284',
+            'pretty_version' => 'v7.0.0',
+            'version' => '7.0.0.0',
+            'reference' => 'c7111310c6116ba508a6a170a89eaaed2129bd42',
             'type' => 'library',
             'install_path' => __DIR__ . '/../phpmailer/phpmailer',
             'aliases' => array(),
