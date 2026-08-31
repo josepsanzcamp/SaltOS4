3a102b8396afd22fc0954acc956b89e0ca8e47fd
Josep Sanz <josep.sanz@saltos.org>
2026-08-31 23:17:18 +0200

    Update tc-lib-pdf to the latest release

--- utest/test_pdf.php
+++ utest/test_pdf.php
@@ -123,16 +123,16 @@ final class test_pdf extends TestCase
 
         $buffer = __pdf_all2pdf('../../utest/files/lorem.html');
         $this->assertStringContainsString('PDF document', get_mime($buffer));
-        $this->assertEqualsWithDelta(40000, strlen($buffer), 1000);
+        $this->assertEqualsWithDelta(38000, strlen($buffer), 1000);
 
         $buffer = __pdf_all2pdf('../../utest/files/lorem.png');
         $this->assertStringContainsString('PDF document', get_mime($buffer));
-        $this->assertEqualsWithDelta(99000, strlen($buffer), 1000);
+        $this->assertEqualsWithDelta(98000, strlen($buffer), 1000);
 
         // Note: this file has the exif rotation to 90º: exiftool -n -Orientation=6 lorem.jpg
         $buffer = __pdf_all2pdf('../../utest/files/lorem.jpg');
         $this->assertStringContainsString('PDF document', get_mime($buffer));
-        $this->assertEqualsWithDelta(289000, strlen($buffer), 1000);
+        $this->assertEqualsWithDelta(288000, strlen($buffer), 1000);
 
         $buffer = __pdf_all2pdf('../../utest/files/lorem.odt');
         $this->assertStringContainsString('PDF document', get_mime($buffer));
